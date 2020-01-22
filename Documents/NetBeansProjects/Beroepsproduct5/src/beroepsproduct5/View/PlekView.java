3ed4d67c488190d9346db7c72ef49bb089dc833c
Sebastiaan Uijlen <sebastiaanuijlen@hotmail.nl>
2020-01-22 13:06:50 +0100

    Jelle added

--- Documents/NetBeansProjects/Beroepsproduct5/src/beroepsproduct5/View/PlekView.java
+++ Documents/NetBeansProjects/Beroepsproduct5/src/beroepsproduct5/View/PlekView.java
@@ -5,13 +5,6 @@
  */
 package beroepsproduct5.View;
 
-import beroepsproduct5.DbConnector;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
@@ -27,36 +20,31 @@ import javafx.scene.text.TextBoundsType;
  * @author SebastiaanU
  */
 public class PlekView extends StackPane {
-    private final ScheduledExecutorService scheduler
-            = Executors.newSingleThreadScheduledExecutor();
-    private DbConnector db = new DbConnector();
 
     private Text temperatuur;
     private Color circleColor;
     private Circle circle = createCircle();
-    private String liveTemp = "test";
     private static int R = 100;
-    private static final Color lineColor = Color.GREY;
+    private static final Color lineColor = Color.WHITE;
 
     final Line l1 = createLine(lineColor, 0, R - 0.5, 2 * R, R - 0.5);
     final Line l2 = createLine(lineColor, R - 0.5, 0, R - 0.5, 2 * R);
 
     public PlekView(Pane p) {
-        temperatuur = new Text(liveTemp);
         temperatuur.setBoundsType(TextBoundsType.VISUAL);
         Group group = new Group(circle, l1, l2);
         this.getChildren().addAll(group, temperatuur);
         p.getChildren().addAll(this);
-        temperatuur.setText(liveTemp);
-        centerText(temperatuur);
 
     }
 
+//methode wordt aangeroepen in tafelview om de kleur van de plek aan te duiden
     public void setCircleColor(Color color){
         circle.setStroke(color);
         circle.setFill(color);
     }
 
+// de circel (tafelplaats) wordt aangemaakt wanneer deze methode wordt aangeroepen
     private Circle createCircle() {
         final Circle circle = new Circle(R);
 
@@ -69,12 +57,7 @@ public class PlekView extends StackPane {
         return circle;
     }
 
-    private void centerText(Text text) {
-        double W = text.getBoundsInLocal().getWidth();
-        double H = text.getBoundsInLocal().getHeight();
-        text.relocate(R - W / 2, R - H / 2);
-    }
-
+//deze lijnen zorgen er voor dat de cirkels op je juiste plek komen te staan.
     private Line createLine(Color lineColor, double x1, double y1, double x2, double y2) {
         Line l1 = new Line(x1, y1, x2, y2);
 
